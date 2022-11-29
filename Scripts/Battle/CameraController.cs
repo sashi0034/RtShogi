992baaa0a964b25c26ac4bf750bdd62caa7c6de5
sashi0034 <satoshi3104.nishi@gmail.com>
2022-11-29 09:01:17 +0900

    CameraControllerを追加

---
+++ Scripts/Battle/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace RtShogi.Scripts.Battle
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Vector3 _destPos;
+        private Camera _camera;
+        private float _moveSpeed = 1;
+        private float _moveUpdateSpeed = 2f;
+        private FloatRange _movableRangeY = new FloatRange(-5f, 5f);
+
+        [EventFunction]
+        private void Start()
+        {
+            _camera = Camera.main;
+            Debug.Assert(_camera!=null);
+            _destPos = _camera.transform.position;
+        }
+
+        [EventFunction]
+        private void Update()
+        {
+            var scroll = Input.mouseScrollDelta;
+            _destPos += new Vector3(0, 0, scroll.y * _moveSpeed);
+            _destPos.z = _movableRangeY.FixInRange(_destPos.z);
+
+            var delta = (_destPos - _camera.transform.position);
+            _camera.transform.position += delta * (Time.deltaTime * _moveUpdateSpeed);
+        }
+    }
+}
---
+++ Scripts/FloatRange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RtShogi.Scripts
+{
+    public struct FloatRange
+    {
+        public readonly float Min;
+        public readonly float Max;
+
+        public FloatRange(float inclusiveMin, float inclusiveMax)
+        {
+            Min = inclusiveMin;
+            Max = inclusiveMax;
+        }
+
+        public bool IsInRange(float value)
+        {
+            return Min <= value && value <= Max;
+        }
+
+        public float FixInRange(float value)
+        {
+            return Math.Min(Math.Max(value, Min), Max);
+        }
+    }
+}
